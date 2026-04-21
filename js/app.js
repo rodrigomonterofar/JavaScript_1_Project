ac4895d4f454f5b9ac1f311cbaa0e6e1552861e5
Rodrigo Montero <rodrigo.montero.farias@gmail.com>
2026-04-21 11:29:01 -0600

    Segundo avance del proyecto

---
+++ js/app.js
@@ -0,0 +1,56 @@
+let egresos = {
+  Renta: 900,
+  Ropa: 400
+};
+
+let ingresos = {
+  Quincena: 9000,
+  Venta: 400
+};
+
+const totalIngresos = () => {
+  let totalIngreso = 0;
+
+  for (let ingreso in ingresos) {
+    totalIngreso += ingresos[ingreso];
+  }
+
+  return totalIngreso;
+};
+
+const totalEgresos = () => {
+  let totalEgreso = 0;
+
+  for (let egreso in egresos) {
+    totalEgreso += egresos[egreso];
+  }
+
+  return totalEgreso;
+};
+
+const formatoMoneda = valor => {
+  return valor.toLocaleString('es-MX', {
+    style: 'currency',
+    currency: 'MXN',
+    minimumFractionDigits: 2
+  });
+};
+
+const formatoPorcentaje = valor => {
+  return valor.toLocaleString('es-MX', {
+    style: 'percent',
+    minimumFractionDigits: 2
+  });
+};
+
+const cargarCabecero = () => {
+  const presupuesto = totalIngresos() - totalEgresos();
+  const porcentajeEgreso = totalEgresos() / totalIngresos();
+
+  console.log(formatoMoneda(presupuesto));
+  console.log(formatoPorcentaje(porcentajeEgreso));
+  console.log(formatoMoneda(totalIngresos()));
+  console.log(formatoMoneda(totalEgresos()));
+};
+
+cargarCabecero();
